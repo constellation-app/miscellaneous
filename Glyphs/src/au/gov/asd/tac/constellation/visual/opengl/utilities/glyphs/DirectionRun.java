e2903454d6ea4e267caa5d25b3331ba805a72fe6
algol60 <algol60@users.noreply.github.com>
2020-01-30 18:52:25 +1100

    Glyphs: update to match CONSTELLATION.

--- Glyphs/src/au/gov/asd/tac/constellation/visual/opengl/utilities/glyphs/DirectionRun.java
+++ Glyphs/src/au/gov/asd/tac/constellation/visual/opengl/utilities/glyphs/DirectionRun.java
@@ -1,17 +1,33 @@
+/*
+ * Copyright 2010-2019 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package au.gov.asd.tac.constellation.visual.opengl.utilities.glyphs;
 
 import java.awt.Font;
 import java.awt.font.TextAttribute;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
- * A run of text that has a single direction (TextAttribute.RUN_DIRECTION_LTR or TextAttribute.RUN_DIRECTION_RTL).
+ * A run of text that has a single direction (TextAttribute.RUN_DIRECTION_LTR or
+ * TextAttribute.RUN_DIRECTION_RTL).
  *
  * @author algol
  */
 class DirectionRun {
+
     final String run;
     final Boolean direction;
 
@@ -19,7 +35,8 @@ class DirectionRun {
      * A new DirectionRun instance.
      *
      * @param run Text that has a single direction.
-     * @param direction One of TextAttribute.RUN_DIRECTION_LTR or TextAttribute.RUN_DIRECTION_RTL.
+     * @param direction One of TextAttribute.RUN_DIRECTION_LTR or
+     * TextAttribute.RUN_DIRECTION_RTL.
      */
     private DirectionRun(final String run, final Boolean direction) {
         this.run = run;
@@ -40,9 +57,9 @@ class DirectionRun {
     /**
      * Return the direction of the specified codepoint, one of 'L', 'R', 'U'.
      * <p>
-     * Uses Character.getDirectionality() to get the directionality.
-     * Codepoints with specific directionality become left 'L' or right 'R',
-     * all other characters are undefined 'U'.
+     * Uses Character.getDirectionality() to get the directionality. Codepoints
+     * with specific directionality become left 'L' or right 'R', all other
+     * characters are undefined 'U'.
      *
      * @param codepoint The Unicode codepoint to get the direction of.
      *
@@ -69,7 +86,8 @@ class DirectionRun {
     }
 
     /**
-     * Find the beginnings and ends of runs of codepoints that have the same direction.
+     * Find the beginnings and ends of runs of codepoints that have the same
+     * direction.
      * <p>
      * If there are any RTL runs, the order of the runs is reversed.
      *
@@ -84,23 +102,23 @@ class DirectionRun {
         final ArrayList<DirectionRun> runs = new ArrayList<>();
 
         int start = 0;
-        for(int offset = 0; offset < length;) {
+        for (int offset = 0; offset < length;) {
             final int codepoint = text.codePointAt(offset);
             final int cc = Character.charCount(codepoint);
 
             final char dir;
             final int cptype = Character.getType(codepoint);
             /*codepoint==32*/
-            if((cptype==Character.SPACE_SEPARATOR || cptype==Character.NON_SPACING_MARK)) {
+            if ((cptype == Character.SPACE_SEPARATOR || cptype == Character.NON_SPACING_MARK)) {
                 dir = currDir;
             } else {
                 final char d = codepointDirection(codepoint);
-                dir = d!='R' ? 'L' : 'R';
+                dir = d != 'R' ? 'L' : 'R';
             }
 //            System.out.printf("codepoint %d %d %c %c %d %s\n", offset, codepoint, currDir, dir, Character.getType(codepoint), Character.UnicodeBlock.of(codepoint));
-            if(dir!=currDir && currDir!=' ') {
+            if (dir != currDir && currDir != ' ') {
 //                System.out.printf("CHDIR %s %s\n", start, offset);
-                runs.add(new DirectionRun(text.substring(start, offset), currDir=='L'?TextAttribute.RUN_DIRECTION_LTR:TextAttribute.RUN_DIRECTION_RTL));
+                runs.add(new DirectionRun(text.substring(start, offset), currDir == 'L' ? TextAttribute.RUN_DIRECTION_LTR : TextAttribute.RUN_DIRECTION_RTL));
                 start = offset;
             }
 
@@ -110,20 +128,64 @@ class DirectionRun {
 
         // Add the end of the final run.
         //
-        runs.add(new DirectionRun(text.substring(start, length), currDir=='L'?TextAttribute.RUN_DIRECTION_LTR:TextAttribute.RUN_DIRECTION_RTL));
+        runs.add(new DirectionRun(text.substring(start, length), currDir == 'L' ? TextAttribute.RUN_DIRECTION_LTR : TextAttribute.RUN_DIRECTION_RTL));
 
         // If there is more than one direction (which implies that at least one
-        // run is right-to-left), or if the first direction is RTL, reverse the runs.
-        // Since what was the final run is now the first run, set the
-        // first direction.
+        // run is right-to-left), modify the order of the runs.
         //
-        if(runs.size()>1 || runs.get(0).direction.equals(TextAttribute.RUN_DIRECTION_RTL)) {
-            Collections.reverse(runs);
+        if (runs.size() > 1) {
+            final DirectionRun run = runs.get(runs.size()-1);
+
+            // If the last run is LTR and ends with a type "<...>", put the type
+            // in its own run and move that run in front of the other runs.
+            // This may mean splitting the run if there is
+            // non-type text present.
+            //
+            // (This is a bit weird, because at this level we have no
+            // knowledge of types, but we're trying to make it look right for
+            // the users.)
+            //
+            if(run.direction.equals(TextAttribute.RUN_DIRECTION_LTR) && run.run.endsWith(">")) {
+                final int lt = run.run.lastIndexOf('<');
+                if(lt>-1) {
+                    // Remove the run; it ends with a type, so it doesn't
+                    // belong at the end.
+                    //
+                    runs.remove(runs.size()-1);
+
+                    if(lt==0) {
+                        // The less-than is at the start of the run,
+                        // therefore the type is already in a run by itself;
+                        // just insert it at the front.
+                        runs.add(0, run);
+                    } else {
+                        // Split the run into two new runs and put them in their
+                        // rightful places.
+                        //
+                        final DirectionRun runType = new DirectionRun(run.run.substring(lt), run.direction);
+                        final DirectionRun runOther = new DirectionRun(run.run.substring(0, lt), run.direction);
+
+                        // The text before the "<" goes back at the end.
+                        //
+                        runs.add(runOther);
+
+                        // If the first run has the same direction, merge the
+                        // type run and the first run, else just insert the
+                        // type run at the front.
+                        //
+                        final DirectionRun run0 = runs.get(0);
+                        if(run0.direction.equals(runType.direction)) {
+                            runs.set(0, new DirectionRun(runType.run + run0.run, runType.direction));
+                        } else {
+                            runs.add(0, runType);
+                        }
+                    }
+                }
+            }
         }
 
 //        System.out.printf("* dir runs %d %s\n", runs.size(), runs.get(0).direction);
 //        runs.stream().forEach(s -> System.out.printf("* dir  run  %d %d [%s]\n", s.run.length(), s.run.codePointAt(0), s));
-
         return runs;
     }
 
